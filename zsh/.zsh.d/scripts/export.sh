33b2ad0e99ecee2e703cfb19fde35958dbb5c99f
Jhonathas Matos <jhonathas@gmail.com>
2019-10-20 12:55:44 -0300

    improvement

--- zsh/.zsh.d/scripts/export.sh
+++ zsh/.zsh.d/scripts/export.sh
@@ -3,25 +3,16 @@ export PATH=$PATH
 
 export EDITOR="nvim"
 
-# brew/cask
-# export HOMEBREW_CASK_OPTS="--caskroom=~/Applications"
-
-# IONIC / CORDOVA
+if [[ "$(uname)" == "Darwin" ]]; then
 export JAVA_HOME=$(/usr/libexec/java_home)
-
-# Using Android Studio path
-export ANDROID_HOME=~/Library/Android/sdk
-export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+fi
 
 export PATH="/usr/local/sbin:$PATH"
 
+if [[ "$(uname)" == "Darwin" ]]; then
 export PATH=$(brew --prefix openssl)/bin:$PATH
+fi
 
-export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
-
-# ExDocker
-
-export PATH="/Users/jhonathas/.asdf/installs/elixir/1.9.1/.mix/escripts:$PATH"
 
 # Stow
 
--- zsh/.zsh.d/scripts/options.sh
+++ zsh/.zsh.d/scripts/options.sh
@@ -3,13 +3,51 @@ setopt +o nomatch
 ulimit -S -n 2048
 
 # Asdf
+if [[ "$(uname)" == "Darwin" ]]; then
 . $(brew --prefix asdf)/asdf.sh
+fi
 
 # Tmuxinator
 source ~/.config/tmuxinator/tmuxinator.zsh
 
-if [ -z "$TMUX" ]; then
-  tmux attach -t default || tmux new -s default
+export HISTFILE=~/.zsh_history
+
+
+unset MAILCHECK # disable "you have mail" warning
+
+os=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+
+# fzf (mac)
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  if [ -z "$TMUX" ]; then
+    tmux attach -t default || tmux new -s default
+  fi
 fi
 
-export HISTFILE=~/.zsh_history
+export LC_ALL=en_US.UTF-8
+# export LC_MESSAGES="C"
+# export LANG=es_US.UTF-8
+
+export EDITOR='nvim'
+export STOW_DIR=$HOME/.dotfiles
+
+if [[ -d "/usr/lib/jvm/java-10-openjdk" ]]; then
+  export JAVA_HOME="/usr/lib/jvm/java-10-openjdk"
+  export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
+fi
+
+if [[ "$os" == "Ubuntu" ]]; then
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
+fi
+
+if [[ "$os" == "\"Manjaro Linux\"" ]]; then
+  source /usr/share/fzf/key-bindings.zsh
+  source /usr/share/fzf/completion.zsh
+fi
+
+alert() {
+  notify-send --icon=gtk-info Alert $1
+}
+
